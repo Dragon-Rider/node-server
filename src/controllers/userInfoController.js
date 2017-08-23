a8539eb9f71b9f1722f1ecda006f8fd8047e236f
JiangHuan <278362241@qq.com>
2017-08-23 11:33:01 +0800

    node降级处理，目前支持6.0.0以上版本

--- src/controllers/userInfoController.js
+++ src/controllers/userInfoController.js
@@ -8,7 +8,7 @@ const cityInfoModel = require('../models/cityInfoModel');
 const getUserinfo = function (ip) {
     const result = {};
     result.ipInfo = ipInfoModel.getIpInfo(ip);
-    result.dbCityList = cityInfoModel.getCityInfo();
+    //result.dbCityList = cityInfoModel.getCityInfo();
 
     return result;
 };
